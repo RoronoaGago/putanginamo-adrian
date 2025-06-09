534237a69cd1869be421e66bcade9ec860dbc741
Harvey <htaningco@gmail.com>
2025-06-09 19:30:06 +0800

    wala akong magagawa

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log("putanignamo");
 console.log("putanginamo adrian");
 console.log("putangina ninyong lahat");
 console.log("Trying out the GIT REVERT");
+console.log("AT KAHIT MAHAL KITA");
